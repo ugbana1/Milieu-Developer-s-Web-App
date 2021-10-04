1006d742ba67e84d0a6750b6b74743dc50e208b6
ugbana1 <okekeisaacs@gmail.com>
2021-10-05 00:08:04 +0100

    My Second Commit for this project

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 # Create your views here.
@@ -14,7 +15,7 @@ def project(request,pk):
     projectObj =Project.objects.get(id=pk)
     return render(request,'projects/single-project.html',{'project':projectObj})
 
-
+@login_required(login_url="login")
 def createProject(request):
      
     form =ProjectForm()
@@ -29,7 +30,7 @@ def createProject(request):
     return render(request,'projects/project_form.html',context)
 
 
-
+@login_required(login_url="login")
 def updateProject(request,pk):
     project =Project.objects.get(id=pk)
     form =ProjectForm(instance=project)
@@ -42,7 +43,7 @@ def updateProject(request,pk):
     context ={'form':form}
     return render(request,'projects/project_form.html',context)
 
-
+@login_required(login_url="login")
 def deleteProject(request,pk):
     project =Project.objects.get(id=pk)
 
